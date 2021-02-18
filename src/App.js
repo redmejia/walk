ac8343dff2bd2e6f6cae3d982583a99d228f4315
redmejia <preynaldorod@gmail.com>
2021-02-17 22:19:09 -0800

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 const  App = () => {
   return (
     <>
+      <NavBar />
       <div className="App">
         <h1>hello, world.!</h1>
       </div>
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <>
+            <h1 className="App">hello word.!</h1>
+            <h3 className="App">this is the NavBar</h3>
+        </>
+    );
+}
+export default NavBar;
