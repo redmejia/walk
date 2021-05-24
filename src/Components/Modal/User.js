ee4e8112f4a5c3d06b5dabcaa85b240c20de9cfb
redmejia <preynaldorod@gmail.com>
2021-05-23 23:14:22 -0700

    register and signin forms

--- src/Components/Modal/User.js
+++ src/Components/Modal/User.js
@@ -1,18 +1,69 @@
+import Register from "../Register/Register"
+import Signin from "../Signin/Signin"
+
 const User = () => {
     return (
         <>
-            <button type="button" class="btn  drop-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" class="btn drop-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{ float: 'right' }}>
                 signin / register
             </button>
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-xl">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            hello world
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-6">
+                                        Signin
+                                        <form >
+                                            <input
+                                                className="form"
+                                                type="text"
+                                                placeholder="email"
+                                            // onChange={onHandle_Email}
+                                            />
+
+                                            <input
+                                                className="form"
+                                                type="password"
+                                                placeholder="password"
+                                                autoComplete="no"
+                                            // onChange={onHandle_Pwd}
+                                            // onKeyDown={userSignin}
+                                            />
+                                        </form>
+                                    </div>
+                                    <div class="col-6">
+                                        Register
+                                        <form >
+                                            <input
+                                                className="form"
+                                                type="text"
+                                                placeholder="name"
+                                            // onChange={onHandle_Email}
+                                            />
+                                            <input
+                                                className="form"
+                                                type="text"
+                                                placeholder="email"
+                                            // onChange={onHandle_Email}
+                                            />
+
+                                            <input
+                                                className="form"
+                                                type="password"
+                                                placeholder="password"
+                                                autoComplete="no"
+                                            // onChange={onHandle_Pwd}
+                                            // onKeyDown={userSignin}
+                                            />
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
